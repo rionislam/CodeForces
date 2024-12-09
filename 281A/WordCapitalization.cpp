c5e7d11e405e7e18d3ebf7701e62c77fb35aa5b0
rionislam <rionislam.mazedul@gmail.com>
2024-12-09 17:57:13 +0600

    Solved 281A Word Capitalization

---
+++ 281A/WordCapitalization.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string a;
+    cin>>a;
+    if(a[0] >= 'a' && a[0] <= 'z'){
+        a[0] = toupper(a[0]);
+    }
+
+    cout<<a;
+    return 0;
+}
