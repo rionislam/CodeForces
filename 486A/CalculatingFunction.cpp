f051fd4878d84949cde7476fb04c3ea84d079128
rionislam <rionislam.mazedul@gmail.com>
2024-12-12 22:11:34 +0600

    Solved 486A

---
+++ 486A/CalculatingFunction.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long n, a;
+    cin>>n;
+
+    if(n%2==0){
+        a = n/2;
+    }else{
+        a = -(n+1)/2;
+    }
+        
+
+    cout<<a;
+    return 0;
+}
