3a49ec7de21bd976c628f06e33b12b2ff2401c66
rionislam <rionislam.mazedul@gmail.com>
2025-01-12 12:39:39 +0600

    Solved 1873A

---
+++ 800/1873A/ShortSort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    string s[t];
+    for(int i=0; i<t; i++){
+        cin>>s[i];
+    }
+
+    for(int i=0; i<t; i++){
+        string swaped = "";
+        if(s[i]=="abc"){
+            cout<<"YES"<<endl;
+        }else{
+            swaped = string(1, s[i][1])+s[i][0]+s[i][2];
+            if(swaped == "abc"){
+                cout<<"YES"<<endl;
+            }else{
+                swaped = string(1,s[i][0])+s[i][2]+s[i][1];
+                if(swaped == "abc"){
+                    cout<<"YES"<<endl;
+                }else{
+                    swaped = string(1,s[i][2])+s[i][1]+s[i][0];
+                    if(swaped == "abc"){
+                        cout<<"YES"<<endl;
+                    }else{
+                        cout<<"NO"<<endl;
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
