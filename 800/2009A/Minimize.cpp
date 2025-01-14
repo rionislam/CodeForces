01c766125b5005e85b701361cee69818fc962159
rionislam <rionislam.mazedul@gmail.com>
2025-01-14 13:02:00 +0600

    Solved 2009A

---
+++ 800/2009A/Minimize.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    int a[t]={0}, b[t]={0};
+
+    for(int i=0; i<t; i++){
+        cin>>a[i]>>b[i];
+    }
+
+    for(int i=0; i<t; i++){
+        int c = b[i];
+        int x = (c-a[i])+(b[i]-c);
+        while(c--){
+            int y = (c-a[i])+(b[i]-c);
+            if(y<x){
+                x=y;
+            }else{
+                break;
+            }
+        }
+        cout<<x<<endl;
+    }
+}
