4488750a5f145bf5ae444cca499075f30a2b9c29
rionislam <rionislam.mazedul@gmail.com>
2025-01-25 12:57:31 +0600

    Solved 1475B

---
+++ 900/1475B/NewYearsNumber.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+
+        if(n/2020>0&&n/2020>=n%2020){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+    
+    return 0;
+}
