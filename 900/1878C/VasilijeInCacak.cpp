b29d96023f1af3653a2ed1e091dbdcdf8a573fa1
rionislam <rionislam.mazedul@gmail.com>
2025-01-30 11:15:31 +0600

    Solved 1878C

---
+++ 900/1878C/VasilijeInCacak.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+
+    while (t--) {
+        long long n, k, x;
+        cin>>n>>k>>x;
+
+        long long minSum = k * (k + 1) / 2;
+        long long maxSum = (n * (n + 1) / 2) - ((n - k) * (n - k + 1) / 2);
+
+        if (x >= minSum && x <= maxSum) {
+            cout<<"YES"<<endl;
+        } else {
+            cout<<"NO"<<endl;
+        }
+    }
+
+    return 0;
+}
