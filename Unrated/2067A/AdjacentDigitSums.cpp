eceba253896757547d954e816e42966e71fdc527
rionislam <rionislam.mazedul@gmail.com>
2025-02-12 11:52:58 +0600

    Solved 2067A

---
+++ Unrated/2067A/AdjacentDigitSums.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y;
+        cin>>x>>y;
+        if (x + 1 >= y && (x + 1 - y) % 9 == 0) {
+            cout<<"Yes"<<endl;
+        } else {
+            cout<<"No"<<endl;
+        }
+    }
+}
