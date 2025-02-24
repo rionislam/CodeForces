4dc615ac0b926a13f535c1fc6662c758f0599a65
rionislam <rionislam.mazedul@gmail.com>
2025-02-24 13:16:12 +0600

    Solved 1238A

---
+++ 900/1238A/PrimeSubtraction.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long x, y;
+        cin>>x>>y;
+        if(x-y==1){
+            cout<<"NO"<<endl;
+        }else{
+            cout<<"YES"<<endl;
+        }
+    }
+    
+    return 0;
+}
