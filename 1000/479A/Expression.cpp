6f37bd1465a108aa03a081093060f3e41bdd26f5
rionislam <rionislam.mazedul@gmail.com>
2025-03-05 12:56:54 +0600

    Solved 479A

---
+++ 1000/479A/Expression.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin>>a>>b>>c;
+
+    int ans = a*b*c;
+    ans = max((a+b)*c, ans);
+    ans = max(a*(b+c), ans);
+    ans = max(a+b+c, ans);
+    ans = max(a+b*c, ans);
+    ans = max(a*b+c, ans);
+
+    cout<<ans<<endl;
+
+    return 0;
+}
