850a30591cea744efc8196e0e18e47196572dca1
rionislam <rionislam.mazedul@gmail.com>
2025-03-17 22:19:27 +0600

    Solved 1521A

---
+++ 1000/1521A/NastiaAndNearlyGoodNumbers.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        long long a, b;
+        cin>>a>>b;
+
+        if(b==1){
+            cout<<"NO"<<endl;
+            continue;
+        }
+
+        cout<<"YES"<<endl;
+        cout<<a<<" "<<a*b<<" "<<a*(b+1)<<endl;
+    }
+
+    return 0;
+}
