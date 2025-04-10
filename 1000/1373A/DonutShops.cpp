85e14bff3f4520a4e513cc1c43295548ee2ccbb0
rionislam <rionislam.mazedul@gmail.com>
2025-04-10 10:47:49 +0600

    Solved 1373A

---
+++ 1000/1373A/DonutShops.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        long long a, b, c;
+        cin>>a>>b>>c;
+
+        if(a<c){
+            cout<<1<<" ";
+        }else{
+            cout<<-1<<" ";
+        }
+
+        if(c<a*b){
+            cout<<b<<endl;
+        }else{
+            cout<<-1<<endl;
+        }
+    }
+
+    return 0;
+}
