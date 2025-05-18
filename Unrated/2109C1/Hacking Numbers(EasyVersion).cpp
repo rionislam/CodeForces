922f926004adb996c3c3a4b535bedfe71844e4a8
rionislam <rionislam.mazedul@gmail.com>
2025-05-18 22:02:55 +0600

    Solved 2109C1 - Slice to Survive

---
+++ Unrated/2109C1/Hacking Numbers(EasyVersion).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin>>n;
+
+        string r;
+
+        cout<<"digit"<<endl;
+        cin>>r; 
+
+        cout<<"mul "<<111111111<<endl;
+        cin>>r;
+
+        cout<<"digit"<<endl;
+        cin>>r;
+
+        cout<<"digit"<<endl;
+        cin>>r;
+
+        cout<<"add "<<n-9<<endl;
+        cin>>r;
+
+        cout<<"!"<<endl;
+        cin>>r; 
+    }
+    return 0;
+}
